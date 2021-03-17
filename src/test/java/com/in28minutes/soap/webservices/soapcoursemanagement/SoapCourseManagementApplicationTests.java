a58f06c2cccdeaed22027590b8755cb26d873a1c
margaret-scheers <margaret.scheers@pobox.com>
2021-03-17 13:05:00 -0500

    Set up for SOAP requests

--- src/test/java/com/in28minutes/soap/webservices/soapcoursemanagement/SoapCourseManagementApplicationTests.java
+++ src/test/java/com/in28minutes/soap/webservices/soapcoursemanagement/SoapCourseManagementApplicationTests.java
@@ -1,8 +1,11 @@
 package com.in28minutes.soap.webservices.soapcoursemanagement;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SoapCourseManagementApplicationTests {
 
